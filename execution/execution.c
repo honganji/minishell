c68a2fa80e3ac8cef9843d9118037155bc62bb50
Yuji Toshihiro <ytoshihi@2-F-2.42heilbronn.de>
2024-05-03 14:49:42 +0200

    implement echo, pwd, and cd command

--- execution/execution.c
+++ execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: ytoshihi <ytoshihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 10:11:47 by ytoshihi          #+#    #+#             */
-/*   Updated: 2024/05/03 13:43:23 by ytoshihi         ###   ########.fr       */
+/*   Updated: 2024/05/03 14:48:23 by ytoshihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,73 @@ static void	ft_execve(t_data *data, char *str)
 		data->args = ft_split(str, ' ');
 		printf("%s part------------------\n", data->args[0]);
 		data->path = ft_strjoin("/bin/", data->args[0]);
-		execve(data->path, data->args, NULL);
+		if (execve(data->path, data->args, NULL) == -1)
+		{
+			printf("Error in execve...");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 		wait(NULL);
 }
 
+static void	ft_chdir(char *path)
+{
+	if (chdir(path) == -1)
+	{
+		printf("Error in chdir...");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void	ft_echo(char *str)
+{
+	char	**arr;
+	int		i;
+	int		n_flag;
+
+	arr = ft_split(str, ',');
+	i = 0;
+	n_flag = 0;
+	// Check if there is -n flag
+	if (!ft_strncmp(str, "-n", 2))
+	{
+		n_flag = 1;
+		i = 1;
+	}
+	// print what you need to display
+	printf("%s", arr[i]);
+	if (n_flag)
+		printf("\n");
+	i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
+}
+
+static void	ft_pwd(void)
+{
+	char	buffer[256];
+	char	*cur_dir;
+
+	cur_dir = getcwd(buffer, sizeof(buffer));
+	if (!cur_dir)
+	{
+		printf("Error in pwd...");
+		exit(EXIT_FAILURE);
+	}
+	printf("%s", cur_dir);
+}
+
 // TODO enable to use env variable
 void	ft_exe_command(t_data *data, char *str, t_com com)
 {
-	if (com == ECHO || com == PWD)
+	if (com == CD)
+		ft_chdir(str);
+	else if (com == ECHO)
+		ft_echo(str);
+	else if (com == PWD)
+		ft_pwd();
+	else
 		ft_execve(data, str);
 }
--- execution/main.c
+++ execution/main.c
@@ -6,7 +6,7 @@
 /*   By: ytoshihi <ytoshihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:55:22 by ytoshihi          #+#    #+#             */
-/*   Updated: 2024/05/03 12:53:48 by ytoshihi         ###   ########.fr       */
+/*   Updated: 2024/05/03 14:49:11 by ytoshihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	main(void)
 	t_data	*data;
 
 	data = (t_data *)ft_calloc(1, sizeof(t_data));
-	ft_exe_command(data, "echo -n I am a hero\n", ECHO);
+	ft_exe_command(data, "-n,\"I am a hero\"\n", ECHO);
 	ft_exe_command(data, "pwd", PWD);
-	ft_exe_command(data, "export NAME=yuji", EXPORT);
-	ft_exe_command(data, "echo $KING\n", ECHO);
+	printf("\n");
+	ft_exe_command(data, "execution", CD);
+	ft_exe_command(data, "pwd", PWD);
 	// system("leaks minishell_exe");
 	return (0);
 }
