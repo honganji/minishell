8fe0e92d260dc5a17c2e8dca769f4abae0c4926d
Yuji Toshihiro <ytoshihi@3-B-8.42heilbronn.de>
2024-05-23 17:02:48 +0200

    modify memory leaks

--- execution/ft_export.c
+++ execution/ft_export.c
@@ -6,11 +6,50 @@
 /*   By: ytoshihi <ytoshihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 14:50:18 by ytoshihi          #+#    #+#             */
-/*   Updated: 2024/05/22 16:27:28 by ytoshihi         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:12:51 by ytoshihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execution.h"
+
+/**
+ * @brief Join two strings safely
+ *
+ * @param str string
+ * @param append string to append
+ * @return char*
+ *
+ */
+char	*safe_strjoin(char *str, char *append)
+{
+	str = ft_free_strjoin(str, append);
+	if (!str)
+		critical_err(strerror(errno));
+	return (str);
+}
+/**
+ * @brief Join env variable list into a string
+ *
+ * @param str string
+ * @param env_list env variable list
+ * @return char*
+ *
+ */
+char	*join_to_export(char *str, t_list *env_list)
+{
+	while (env_list)
+	{
+		str = safe_strjoin(str, "declare -x");
+		str = safe_strjoin(str, (*(t_env *)(env_list->content)).key);
+		str = safe_strjoin(str, "=");
+		str = safe_strjoin(str, "\"");
+		str = safe_strjoin(str, (*(t_env *)(env_list->content)).value);
+		str = safe_strjoin(str, "\"");
+		str = safe_strjoin(str, "\n");
+		env_list = env_list->next;
+	}
+	return (str);
+}
 
 /**
  * @brief export value into env linked list
@@ -24,17 +63,39 @@ void	ft_export(t_data *data)
 	char	**env_arr;
 	t_env	*env_json;
 	t_list	*targ_lst;
+	char	*arg;
+	t_list	*tmp;
 
-	env_arr = (char **)ft_calloc(1, sizeof(char *));
-	env_json = (t_env *)malloc(1 * sizeof(t_env));
+	arg = ft_strdup("");
+	if (!((t_cmd *)data->cmd_lst->content)->args[1])
+	{
+		tmp = data->env_lst;
+		arg = join_to_export(arg, tmp);
+		ft_input_data(data, arg, 0);
+		return ;
+	}
+	arg = ((t_cmd *)data->cmd_lst->content)->args[1];
+	if (!ft_strchr(arg, '='))
+	{
+		ft_input_data(data, "", 0);
+		return ;
+	}
+	env_arr = (char **)ft_calloc(2, sizeof(char *));
+	env_json = (t_env *)malloc(sizeof(t_env));
 	if (!env_arr || !env_json)
 		critical_err(strerror(errno));
-	if (!((t_cmd *)data->cmd_lst->content)->args[1])
-		return ;
-	ft_to_json(env_arr, ((t_cmd *)data->cmd_lst->content)->args[1]);
+	ft_to_json(env_arr, arg);
 	env_json->key = env_arr[0];
+	if (ft_isdigit(*env_json->key))
+	{
+		syntax_err(data, "export: not a valid identifier: ", env_json->key, 1);
+		free(env_arr);
+		free(env_json);
+		ft_input_data(data, "", 0);
+		return ;
+	}
 	env_json->value = env_arr[1];
-	targ_lst = ft_find_ele(data, env_arr[0]);
+	targ_lst = ft_find_ele(data, env_json->key);
 	if (targ_lst)
 	{
 		free((*(t_env *)targ_lst->content).key);
