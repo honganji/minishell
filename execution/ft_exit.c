86b6b49a96d35825192e4ebe9e95350b322e5c0c
Yuji Toshihiro <ytoshihi@2-C-4.42heilbronn.de>
2024-05-25 14:05:36 +0200

    modify exit function

--- execution/ft_exit.c
+++ execution/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: ytoshihi <ytoshihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:31:12 by ytoshihi          #+#    #+#             */
-/*   Updated: 2024/05/25 10:14:55 by ytoshihi         ###   ########.fr       */
+/*   Updated: 2024/05/25 14:05:17 by ytoshihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	ft_exit(int code, t_data *data, int is_cmd)
 {
 	if (is_cmd)
-		free_env_lst(data);
-	free_cmd_lst(data);
+		free_cmd_lst(data);
+	free_env_lst(data);
 	free(data);
 	// system("leaks minishell");
 	exit(code);
